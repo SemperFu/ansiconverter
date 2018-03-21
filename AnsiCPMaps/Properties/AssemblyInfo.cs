ce834f01d7c661f4bb6750cc247fea42c60d5019
Jason Windisch <Jason.Windisch@Gmail.com>
2018-03-21 13:17:05 -0400

    Assembly issues fixed

--- AnsiCPMaps/Properties/AssemblyInfo.cs
+++ AnsiCPMaps/Properties/AssemblyInfo.cs
@@ -1,25 +1,24 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-// Review the values of the assembly attributes
-
 [assembly: AssemblyTitle("ANSI_ASCII_Converter_ANSICPMaps")]
 [assembly: AssemblyDescription("Converter for ASCII, ANSI, PCB, BIN Text to HTML, Unicode and back")]
 [assembly: AssemblyCompany("Carsten Cumbrowski aka Roy/SAC")]
 [assembly: AssemblyProduct("ANSI-ASCII-Converter")]
 [assembly: AssemblyCopyright("Copyleft 2012")]
 
-[assembly: AssemblyTrademark("")]
-
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-//The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("0103965d-644a-4e97-82db-2b83647316af")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cba06dc8-efdc-4dc3-be73-a588e733610a")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -27,9 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// <Assembly: AssemblyVersion("1.0.*")> 
-
 [assembly: AssemblyVersion("1.04.0.0")]
 [assembly: AssemblyFileVersion("1.04.0.0")]
--- Converter/Properties/AssemblyInfo.cs
+++ Converter/Properties/AssemblyInfo.cs
@@ -1,25 +1,24 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-// Review the values of the assembly attributes
-
 [assembly: AssemblyTitle("ANSI_ASCII_Converter_Lib")]
 [assembly: AssemblyDescription("Converter for ASCII, ANSI, PCB, BIN Text to HTML, Unicode and back")]
 [assembly: AssemblyCompany("ANSI-ASCII-Converter")]
 [assembly: AssemblyProduct("ANSI-ASCII-Converter")]
 [assembly: AssemblyCopyright("Copyleft 2012")]
 
-[assembly: AssemblyTrademark("")]
-
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-//The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("ebd18a6c-c979-4ba9-b0c6-0e89402a3210")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c315715e-2f38-45cb-9112-b273ec1dcb5b")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -27,9 +26,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// <Assembly: AssemblyVersion("1.0.*")> 
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("1.04.0.0")]
-[assembly: AssemblyFileVersion("1.04.0.0")]
